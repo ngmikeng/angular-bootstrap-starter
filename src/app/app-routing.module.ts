7800017e168d6eef5cf3bcd232366d8d5eecc19a
Minh Nguyen <ngmikeng@gmail.com>
2019-06-16 14:24:42 +0700

    Add auth module, login page, update styles.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
   { path: '**', redirectTo: 'pages' },
 ];
 
